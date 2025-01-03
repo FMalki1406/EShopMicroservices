308272b55f0d8dcef8c63b6e09329451c968238d
Abdullah alsaiari <A.alsaiari@techtrack.sa>
2025-01-03 19:58:47 +0300

    Create Catalog

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// add services
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+// configure Http request pipline
+app.MapCarter();
+
+app.Run();
